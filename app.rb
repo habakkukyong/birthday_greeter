629a17b70e7365c2c7a052582287d3be16b096fa
habakkukyong <habakkukyong@gmail.com>
2019-01-24 11:59:30 +0000

    Pass test for passing of name to greeting page

--- app.rb
+++ app.rb
@@ -3,8 +3,20 @@ require 'sinatra'
 
 # Starts a server that responds to get request with test view
 class BirthdayGreeter < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
+  end
+
+  post '/greeting' do
+    session[:name] = params[:name]
+    redirect '/greeting'
+  end
+
+  get '/greeting' do
+    @name = session[:name]
+    erb :greeting
   end
 
   # start the server if ruby file executed directly
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,10 +1,16 @@
 require 'capybara/dsl'
 require 'selenium-webdriver'
 
-feature 'BirthdayGreeter' do
+feature 'Index' do
   scenario 'Get request to root loads content' do
     visit('/')
-    puts page.html
-    expect(page).to have_content 'Happy Birthday'
+    expect(page).to have_content 'Name?'
+  end
+
+  scenario 'Username is passed to greeting page' do
+    visit('/')
+    fill_in 'name', with: 'username'
+    click_button 'Submit'
+    expect(page).to have_content 'Happy Birthday username!'
   end
 end
